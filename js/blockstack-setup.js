7c4e69d2b8ee9fc70a4d78e33bf7e6c403b1f707
genderev <60559370+genderev@users.noreply.github.com>
2020-03-10 01:40:22 +0000

    changes

--- js/blockstack-setup.js
+++ js/blockstack-setup.js
@@ -1,12 +1,17 @@
-const appConfig = new blockstack.AppConfig(["store_write", "publish_data"])
-
-appConfig.redirectURI = () => "home.html";
+var appConfig = new blockstack.AppConfig(
+  ['email', 'publish_data', 'store_write'], // app access scopes
+  // You also can pass
+  // appDomain
+  // redirectPath
+  // manifestPath 
+);
 
 var userSession = new blockstack.UserSession({
- appConfig,
-});
+  appConfig,
+}); 
 
 function signOut() {
   // We pass url to redirect after signing out
   blockstack.signUserOut('index.html');
 }
+
