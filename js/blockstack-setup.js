730313e29046a270b04efb6d6d3123f279299010
genderev <60559370+genderev@users.noreply.github.com>
2020-03-10 01:27:44 +0000

    testing usersession stuff

--- js/blockstack-setup.js
+++ js/blockstack-setup.js
@@ -1,10 +1,10 @@
 const appConfig = new blockstack.AppConfig(["store_write", "publish_data"])
 
-// appConfig.redirectURI = () => "home.html";
+appConfig.redirectURI = () => "home.html";
 
-// var userSession = new blockstack.UserSession({
-//  appConfig,
-// });
+var userSession = new blockstack.UserSession({
+ appConfig,
+});
 
 function signOut() {
   // We pass url to redirect after signing out
