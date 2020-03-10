265ea0cde829b5011285525a4187cccb07d95a49
genderev <60559370+genderev@users.noreply.github.com>
2020-03-10 01:56:37 +0000

    Update blockstack-setup.js

--- js/blockstack-setup.js
+++ js/blockstack-setup.js
@@ -1,9 +1,6 @@
 // var appConfig = new blockstack.AppConfig(
 //   ['email', 'publish_data', 'store_write'], // app access scopes
-//   // You also can pass
-//   // appDomain
-//   // redirectPath
-//   // manifestPath 
+  
 // );
 
 // var userSession = new blockstack.UserSession({
