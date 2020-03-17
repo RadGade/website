d726a16e21987d2ecaa799b47a88716fab8b55b6
genderev <60559370+genderev@users.noreply.github.com>
2020-03-17 03:03:26 +0000

    Update blockstack-setup.js

--- js/blockstack-setup.js
+++ js/blockstack-setup.js
@@ -2,7 +2,7 @@ var appConfig = new blockstack.AppConfig(['email', 'publish_data', 'store_write'
 
 appConfig.appDomain = () => "https://genderev.com";
 appDomain = () => "https://genderev.com";
-appDomain = "https://genderev.com";
+const appDomain = "https://genderev.com";
   
 
 var userSession = new blockstack.UserSession({
