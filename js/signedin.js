cfb0f9d7b9be58a2f7125619af94a5ed25216d9d
v <>
2020-03-25 10:50:20 -0400

    speeding up site
    gzip files

--- js/signedin.js
+++
@@ -1,9 +0,0 @@
-// We assume that user is signed in in this file
-
-
-window.addEventListener('load', (event) => {
-  if (!userSession.isUserSignedIn()) {
-    alert('Error: user is not signed in');
-  }
-
-});
