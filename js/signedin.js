a95b286a803b43b72e7fee136448620ee973027d
genderev <60559370+genderev@users.noreply.github.com>
2020-03-25 15:13:17 +0000

    Update signedin.js

--- js/signedin.js
+++ js/signedin.js
@@ -1,9 +1,5 @@
-// We assume that user is signed in in this file
-
-
 window.addEventListener('load', (event) => {
   if (!userSession.isUserSignedIn()) {
     alert('Error: User is not signed in.');
   }
-
 });
