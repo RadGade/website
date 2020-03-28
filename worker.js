c42a2c170c78f866d9b27684c012c8432affbbcc
genderev <60559370+genderev@users.noreply.github.com>
2020-03-28 23:03:16 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-importScripts('https://unpkg.com/blockstack@21.0.0/dist/blockstack.js', '../workly/workly.js');
+importScripts('https://unpkg.com/blockstack@21.0.0/dist/blockstack.js', 'js/workly/workly.js');
 
 const appConfig = new blockstack.AppConfig(['store_write', 'publish_data'])
 // const appDomain = "https://genderev.com";
