3d994c42d69c28a42367dfc59b62632c8e7b4cfd
genderev <60559370+genderev@users.noreply.github.com>
2020-03-28 23:03:27 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 importScripts('https://unpkg.com/blockstack@21.0.0/dist/blockstack.js', 'js/workly/workly.js');
 
 const appConfig = new blockstack.AppConfig(['store_write', 'publish_data'])
-// const appDomain = "https://genderev.com";
+const appDomain = "https://genderev.com";
 var userSession = new blockstack.UserSession({
 appConfig,
 });
 
 workly.expose(appConfig);
-// workly.expose(appDomain);
+workly.expose(appDomain);
 workly.expose(UserSession);
