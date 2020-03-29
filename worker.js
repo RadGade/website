b75565ecd5c7ec7c317deed6f36ea2c67bd2d949
genderev <60559370+genderev@users.noreply.github.com>
2020-03-29 03:30:50 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-import { AppConfig, UserSession } from 'js/blockstack.js';
+importScripts('js/blockstack.js');
 addEventListener('message', e => {
  postMessage(const appConfig = new blockstack.AppConfig(['store_write', 'publish_data']);var userSession = new blockstack.UserSession({appConfig,});
  );
