08de3a51899b1ce077fc4982d26ed0fdf8484199
genderev <60559370+genderev@users.noreply.github.com>
2020-03-29 03:24:49 +0000

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,5 @@
+import { sayHello } from 'js/blockstack.js';
+addEventListener('message', e => {
+ postMessage(const appConfig = new blockstack.AppConfig(['store_write', 'publish_data']);const appDomain = "https://genderev.com";var userSession = new blockstack.UserSession({appConfig,});
+ );
+});
